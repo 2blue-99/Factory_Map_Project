f16961a64c1840f102baf7c62fe030804ade6990
LeePureum <lpm083100@gmail.com>
2025-02-12 17:58:05 +0900

    데이터 필터 적용 로직 1

--- data/src/main/java/com/example/data/repo/FactoryRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repo/FactoryRepositoryImpl.kt
@@ -3,12 +3,13 @@ package com.example.data.repo
 import com.example.data.Mapper.toEntity
 import com.example.data.datastore.UserDataStore
 import com.example.data.local.dao.FactoryDao
+import com.example.data.local.dao.FilterDao
 import com.example.data.remote.datasource.GyeonggiDataSourceImpl
 import com.example.data.remote.util.toDomain
-import com.example.domain.type.AreaType
 import com.example.domain.model.FactoryInfo
 import com.example.domain.model.GyeonggiInfo
 import com.example.domain.repo.FactoryRepository
+import com.example.domain.type.AreaType
 import com.example.domain.util.GYEONGGI_DOWNLOAD_COUNT
 import com.example.domain.util.ResourceState
 import kotlinx.coroutines.Dispatchers
@@ -17,12 +18,12 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
-import timber.log.Timber
 import javax.inject.Inject
 
 class FactoryRepositoryImpl @Inject constructor(
     private val gyeonggiDataSource: GyeonggiDataSourceImpl,
     private val factoryDao: FactoryDao,
+    private val filterDao: FilterDao,
     private val userDataSource: UserDataStore
 ): FactoryRepository {
     override fun getGyeonggiData(): Flow<ResourceState<List<GyeonggiInfo>>> {
@@ -65,16 +66,31 @@ class FactoryRepositoryImpl @Inject constructor(
         return factoryDao.getAllData().map { it.map { it.toDomain() } }
     }
 
-
+// ("A", "1") ("A", "2") ("A", "3") ("B", "4") ("B", "5")
+// [("A", ["1","2","3"]), ("B", ["4","5"])]
     override suspend fun getFactoryDao(): Flow<List<FactoryInfo>> {
-        val target = AreaType.toType(userDataSource.areaPositionFlow.first()).title
-        Timber.d("target : $target")
-        return factoryDao.getTargetData(target).map { it.map { it.toDomain() } }
-    }
 
-//    override fun getFactoryDao(): Flow<List<FactoryInfo>> {
-//        return factoryDao.getAllData().map { it.map { it.toDomain() } }
-//    }
+        val targetArea = AreaType.toType(userDataSource.areaPositionFlow.first()).title
+        val filterList = filterDao.getAllData().first()
+        val targetFilterList = filterList.map { it.target }.toSet().toList()
+        val gap = filterList.filter { it.target == targetFilterList[0] }.map { it.keyword }
+
+        val mapList = filterList.map { it.target }
+            .toSet()
+            .map { key ->
+                key to filterList.filter { it.target == key }.map { it.target }
+            }
+        for((key, list) in mapList){
+            key
+        }
+
+
+        val keywordFilterList = filterList.map { it.keyword }
+        val entireList = factoryDao.getTargetData(targetArea).map { it.map { it.toDomain() } }
+
+
+        return entireList
+    }
 
     override suspend fun upsertFactoryDao(data: FactoryInfo) {
         factoryDao.upsertData(data.toEntity())
